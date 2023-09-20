db4e0c4b81c4217c86b2ded1c2754c4f5318b7c7
Andrés Zamora <azviquez93@icloud.com>
2023-09-20 15:03:48 -0600

    User added to domain layer

---
+++ src/main/java/cleanArchitectureExample/domain/User.java
@@ -0,0 +1,15 @@
+package cleanArchitectureExample.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class User {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String password;
+}
